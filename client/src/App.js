560d4df5de9b0f87a1386d2b22611a1e7c714e77
Abdurehman339 <abdurrehman.musharaf@gamil.com>
2023-08-29 02:02:55 +0500

    Added react-router-dom and create path

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
 import ShoppingCategory from './Pages/ShoppingCategory';
@@ -6,12 +5,22 @@ import Product from './Pages/Product';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import Cart from './Pages/Cart';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Signup/>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/shoppingcategory" element={<ShoppingCategory/>}/>
+          <Route path="/product" element={<Product/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
